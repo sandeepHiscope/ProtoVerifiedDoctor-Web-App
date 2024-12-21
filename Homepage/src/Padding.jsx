d09fd87c52dd44f80abf9c26a67ab418f99311b0
kiranvenkat12 <beesettyk@gmail.com>
2024-12-21 17:08:16 +0530

    Created project and added homepage

---
+++ Homepage/src/Padding.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Dropdown from "./Dropdown"
+
+
+const MovingText = () => {
+  return (
+    <>
+    <Dropdown />
+    <div className="moving-text-container">
+      <p className="moving-text">
+        With our Verified Doctor app, your health is always within reach.
+      </p>
+    </div>
+    </>
+  );
+};
+
+export default MovingText;
---
+++ Homepage/src/main.jsx
@@ -0,0 +1,16 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Index from "./Index"
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  
+    <Index />
+  
+  
+)
+
+

6e5dcd739142e0d616f29cf0a2329e506a2c85ef
sandeephiscopetech <sandeep.m@hiscopetech.com>
2024-12-26 19:05:25 +0530

    worked with logos success trail

--- src/Home.jsx
+++ src/Home.jsx
@@ -453,48 +453,46 @@ function Homepage() {
             </div>
           </div>
         </div>
-        {/* lastpart */}
-      </>
-      <div className="body4">
-        <div className="container4">
-          <div className="preview">
-            {/* If the image is in the public folder, use relative path */}
+        /* lastpart */}
+          </>
+          <div className="body4">
+            <div className="container4">
+              <div className="preview">
             <img
-              src="/assets/Images/VDr-logo.png"
+              src={VDrLogo}
               height="400px"
               width="900px"
               alt="App Preview"
             />
-          </div>
-          <div className="info">
+              </div>
+              <div className="info"></div>
             <h2 className="title">Download VDr app</h2>
-            <p className="description">
+            <p className="description"></p>
               Consult with India's top doctors via video on VDr app. Get 24/7
               access to healthcare.
             </p>
             <div className="store-buttons">
               <div className="store-button">
-                {/* Make sure to place the image in the public folder or import them directly */}
                 <img
-                  src="./assets/icons/apps/googleplay.jpeg"
-                  alt="Google Play"
-                  style={{ width: "30px", borderRadius: "20px" }}
+              src={GooglePlayLogo}
+              alt="Google Play"
+              style={{ width: "30px", borderRadius: "20px" }}
                 />
                 <span>Play Store</span>
               </div>
               <div className="store-button">
                 <img
-                  src="./assets/icons/apps/appstore.jpeg" 
-                  alt="Apple App Store"
-                  style={{ width: "30px", borderRadius: "20px" }}
+              src={AppStoreLogo}
+              alt="Apple App Store"
+              style={{ width: "30px", borderRadius: "20px" }}
                 />
                 <span>App Store</span>
               </div>
             </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      {/* Footer */}
+          {/* Footer */}
       
     </>
   );

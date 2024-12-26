00eb5d444990b86e7ac881d221e639b52f50690b
sandeephiscopetech <sandeep.m@hiscopetech.com>
2024-12-27 00:02:26 +0530

    made changes to doc verify

--- src/doctorVerificationpage.jsx
+++ src/doctorVerificationpage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./doctorVerificationpage.css";
+import imgx from "../src/assets/Images/Cardiologist.png";
 // import DoctorVerification from "./doctorVerificationpage";
 import FindDoctorPage from "./findDoctorPage";
 import Home from "./Home";
@@ -7,6 +8,8 @@ import Login from "./loginPage";
 import IndividualRegisterPage from "./individualRegisterPage";
 import SosPage from "./sosPage";
 import DoctorRegisterPage from "./doctorRegisterPage";
+import MainHeader from "./header";
+import Footer from "./footer";
 
 const DoctorVerification = () => {
   const [fullName, setFullName] = useState("");
@@ -32,156 +35,13 @@ const DoctorVerification = () => {
   };
 
   return (
-    <div className="main-container">
-      <div className="logo">
-        <a href="#">
-          <img src="./assets/" alt="logo" />
-        </a>
-      </div>
-      <div className="frame-3">
-        <div className="find-a-doctor">
-          <div className="find-a-doctor2">Find a Doctor</div>
-        </div>
-        <div className="verify-a-doctor2">
-          <div className="verify-a-doctor3">Verify a Doctor</div>
-        </div>
-        <div className="sos">
-          <div className="sos2">SOS</div>
-        </div>
-        <div className="home-delivery-medicines">
-          <div className="home-delivery-medicines2">
-            Home Delivery Medicines
-          </div>
-        </div>
-        <div className="micro-insurance">
-          <div className="micro-insurance2">Micro Insurance</div>
-        </div>
-        <div className="regular-insurance">
-          <div className="regular-insurance2">Regular Insurance</div>
-        </div>
-      </div>
-      <div className="login-signup">
-        <div className="rectangle-1"></div>
-        <div className="login-signup2">Login / Signup</div>
-      </div>
-      <div className="rectangle-16"></div>
-      <div className="div"></div>
 
-      <div className="footer-cont">
-        <div className="logo">
-          <a href="#">
-            <img src="./assets/" alt="logo" />
-          </a>
-        </div>
-        <div className="about-us">
-          <div className="about-us2">About Us</div>
-        </div>
-        <div className="blog">
-          <div className="blog2">Blog</div>
-        </div>
-        <div className="careers">
-          <div className="careers2">Careers</div>
-        </div>
-        <div className="press">
-          <div className="press2">Press</div>
-        </div>
-        <div className="contact-us">
-          <div className="contact-us2">Contact Us</div>
-        </div>
-        <div className="for-patient">
-          <div className="for-patient2">For Patient</div>
-        </div>
-        <div className="search-for-doctors">
-          <div className="search-for-doctors2">Search for Doctors</div>
-        </div>
-        <div className="search-for-clinics">
-          <div className="search-for-clinics2">Search for Clinics</div>
-        </div>
-        <div className="search-for-hospitals">
-          <div className="search-for-hospitals2">Search for Hospitals</div>
-        </div>
-        <div className="v-dr-plus">
-          <div className="v-dr-plus2">VDr Plus</div>
-        </div>
-        <div className="read-health-articles">
-          <div className="read-health-articles2">Read Health Articles</div>
-        </div>
-        <div className="read-about-medicines">
-          <div className="read-about-medicines2">Read About Medicines</div>
-        </div>
-        <div className="v-dr-drive">
-          <div className="v-dr-drive2">VDr Drive</div>
-        </div>
-        <div className="health-app">
-          <div className="health-app2">Health App</div>
-        </div>
-        <div className="for-doctors">
-          <div className="for-doctors2">For Doctors</div>
-        </div>
-        <div className="v-dr-profile">
-          <div className="v-dr-profile2">VDr Profile</div>
-        </div>
-        <div className="for-clinics">
-          <div className="for-clinics2">For Clinics</div>
-        </div>
-        <div className="v-dr-pro">
-          <div className="v-dr-pro2">VDr Pro</div>
-        </div>
-        <div className="v-dr-reach">
-          <div className="v-dr-reach2">VDr Reach</div>
-        </div>
-        <div className="ray-by-v-dr">
-          <div className="ray-by-v-dr2">Ray by VDr</div>
-        </div>
-        <div className="ray-tab">
-          <div className="ray-tab2">Ray Tab</div>
-        </div>
-        <div className="for-hospitals">
-          <div className="for-hospitals2">For Hospitals</div>
-        </div>
-        <div className="insta-by-v-dr">
-          <div className="insta-by-v-dr2">Insta by VDr</div>
-        </div>
-        <div className="qikwell-by-v-dr">
-          <div className="qikwell-by-v-dr2">Qikwell by VDr</div>
-        </div>
-        <div className="v-dr-profile3">
-          <div className="v-dr-profile2">VDr Profile</div>
-        </div>
-        <div className="v-dr-reach3">
-          <div className="v-dr-reach4">VDr Reach</div>
-        </div>
-        <div className="v-dr-drive3">
-          <div className="v-dr-drive4">VDr Drive</div>
-        </div>
-        <div className="more">
-          <div className="more2">More</div>
-        </div>
-        <div className="help">
-          <div className="help2">Help</div>
-        </div>
-        <div className="developers">
-          <div className="developers2">Developers</div>
-        </div>
-        <div className="privacy-policy">
-          <div className="privacy-policy2">Privacy Policy</div>
-        </div>
-        <div className="pcs-t-c">
-          <div className="pcs-t-c2">PCS T&C</div>
-        </div>
-        <div className="terms-conditions">
-          <div className="terms-conditions2">Terms & Conditions</div>
-        </div>
-        <div className="v-dr-health-wiki">
-          <div className="v-dr-health-wiki2">VDr Health Wiki</div>
-        </div>
-        <div className="healthcare-directory">
-          <div className="healthcare-directory2">Healthcare Directory</div>
-        </div>
-        <div className="v-drapp">VDrapp</div>
-      </div>
+    <>
+    <MainHeader />
+    <>
+    <div className="main-container">
       <div className="main-img">
-        <img src="" alt="main-img" />
+        <img src={imgx} alt="main-img" />
       </div>
       <div className="verification-cont">
         <div className="doctor-verification">Doctor Verification</div>
@@ -345,6 +205,9 @@ const DoctorVerification = () => {
         </div>
       </div>
     </div>
+    </>
+    <Footer/>
+    </>
   );
 };
 

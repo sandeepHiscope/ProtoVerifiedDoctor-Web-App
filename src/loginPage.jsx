88963e2e2f4f1c774a49f4d6ab537515c8b8a5a2
kiranvenkat12 <beesettyk@gmail.com>
2024-12-27 16:08:09 +0530

    added footer to login page

--- src/loginPage.jsx
+++ src/loginPage.jsx
@@ -56,7 +56,21 @@ const Login = () => {
           </div>
         </div>
       </main>
+      <footer className="login-footer">
+<img
+  src="https://cdn.builder.io/api/v1/image/assets/TEMP/8c7c2f1f7f0ad4e4188183ac4b58840bac63df589165099f22e5a8c9c8da274d?placeholderIfAbsent=true&apiKey=9ccc22c2724c427c8498a732bb366bf4"
+  alt=""
+  className="sos-background-image-footer"/>
 
+  <div className="sos-footer-div1">
+  <img
+          src="./public/vdr_2-removebg-preview-removebg-preview.png"
+          alt="VDrapp Footer Logo"
+          className="sos-footer-logo"
+        />
+ </div>
+ <div className="sos-footer-copyright"><h6> @Hiscope 2024 </h6></div>
+</footer>
     </>
   );
 }

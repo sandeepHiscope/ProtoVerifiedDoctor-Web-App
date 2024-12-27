0c0a176375e99dad26d7bc8e5b09220840f5fcbb
PRAVEEN-PUVVALA <praveen.p@hiscopetech.com>
2024-12-27 15:01:15 +0530

    header and footer alignment

--- src/doctorVerificationpage.jsx
+++ src/doctorVerificationpage.jsx
@@ -9,7 +9,6 @@ import IndividualRegisterPage from "./individualRegisterPage";
 import SosPage from "./sosPage";
 import DoctorRegisterPage from "./doctorRegisterPage";
 import MainHeader from "./header";
-import Footer from "./footer";
 
 const DoctorVerification = () => {
   const [fullName, setFullName] = useState("");
@@ -38,7 +37,7 @@ const DoctorVerification = () => {
 
     <>
     <MainHeader />
-    <>
+   
     <div className="main-container">
       <div className="main-img">
         <img src={imgx} alt="main-img" />
@@ -205,8 +204,23 @@ const DoctorVerification = () => {
         </div>
       </div>
     </div>
-    </>
-    <Footer/>
+
+       {/* Footer */}
+       <footer className="doctorVerify-footer">
+<img
+  src="https://cdn.builder.io/api/v1/image/assets/TEMP/8c7c2f1f7f0ad4e4188183ac4b58840bac63df589165099f22e5a8c9c8da274d?placeholderIfAbsent=true&apiKey=9ccc22c2724c427c8498a732bb366bf4"
+  alt=""
+  className="doctorVerify-background-image-footer"/>
+
+  <div className="doctorVerify-footer-div1">
+  <img
+          src="./public/vdr_2-removebg-preview-removebg-preview.png"
+          alt="VDrapp Footer Logo"
+          className="doctorVerify-footer-logo"
+        />
+ </div>
+ <div className="doctorVerify-footer-copyright"><h6> @Hiscope 2024 </h6></div>
+</footer>
     </>
   );
 };
--- src/sosPage.jsx
+++ src/sosPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from "react";
 import "./sosPage.css";
 import Header from "./header";
-import Footer from "./footer";
 
 import DoctorVerification from "./doctorVerificationpage";
 import FindDoctorPage from "./findDoctorPage";
@@ -56,6 +55,7 @@ const SosPage = () => {
     <Header/>
 
       {/* SOS part */}
+      <div className="sos-container">
       <div className="div1">
         <button className="div1_btn" onDoubleClick={handleDoubleClick}>
           {count}
@@ -67,10 +67,29 @@ const SosPage = () => {
           <button className="stop_btn" onClick={stopSos}>
             X
           </button>
+     
         </>
+     
       )}
+    </div>
 
-      <Footer/>
+
+    {/* Footer */}
+    <footer className="sos-footer">
+<img
+  src="https://cdn.builder.io/api/v1/image/assets/TEMP/8c7c2f1f7f0ad4e4188183ac4b58840bac63df589165099f22e5a8c9c8da274d?placeholderIfAbsent=true&apiKey=9ccc22c2724c427c8498a732bb366bf4"
+  alt=""
+  className="sos-background-image-footer"/>
+
+  <div className="sos-footer-div1">
+  <img
+          src="./public/vdr_2-removebg-preview-removebg-preview.png"
+          alt="VDrapp Footer Logo"
+          className="sos-footer-logo"
+        />
+ </div>
+ <div className="sos-footer-copyright"><h6> @Hiscope 2024 </h6></div>
+</footer>
           </>
   );
 };
